a5531d2d880b8ee93cb1b9ad309bba7ee6859b4d
locallab-seoul <locallab.seoul@gmail.com>
2022-09-25 23:42:13 +0900

    urls and views update for 4 about pages

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('greeting/', views.greeting, name='greeting'),
+    path('facility/', views.facility, name='facility'),
+    path('history/', views.history, name='history'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -7,3 +7,15 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def greeting(request):
+    return render(request, 'greeting.html')
+
+def facility(request):
+    return render(request, 'facility.html')
+
+def history(request):
+    return render(request, 'history.html')
+
+def contact(request):
+    return render(request, 'contact.html')
