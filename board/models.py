97cd9390059c3607ce00d6019822e785059432ed
locallab-seoul <locallab.seoul@gmail.com>
2022-09-26 00:24:51 +0900

    make test board model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Notice(models.Model):
+
+    title       = models.CharField(max_length=200, verbose_name="제목")
+    contents    = models.TextField(verbose_name="내용")
+    created_at  = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
+    updated_at  = models.DateTimeField(auto_now=True, verbose_name="최종수정일")
+    hit = models.PositiveIntegerField(default = 0)
+
+    def __str__(self):
+        return self.title
