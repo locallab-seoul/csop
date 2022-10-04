79e1f7b0f433323fc7442b004027db37f4d9cf14
locallab-seoul <locallab.seoul@gmail.com>
2022-10-05 08:00:43 +0900

    inital connect database (board-list)

--- board/views.py
+++ board/views.py
@@ -1,6 +1,9 @@
+from tkinter.messagebox import NO
 from django.shortcuts import render
+from .models import Notice
 
 # Create your views here.
 
 def board_list(request):
-    return render(request, 'board-list.html')
+    all_boards  = Notice.objects.all().order_by('-id')
+    return render(request, 'board-list.html', {'notices': all_boards})
